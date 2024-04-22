90b75132f450a799c8f23361def2536c4f4eec6c
simbahebinbo <lansheng228@163.com>
2024-04-22 11:17:10 +0800

    从 domicon 读取数据

--- das/domicon_storage_service.go
+++ das/domicon_storage_service.go
@@ -7,7 +7,6 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/log"
 	"github.com/offchainlabs/nitro/arbstate"
-	"github.com/offchainlabs/nitro/util/pretty"
 	"net/http"
 	"time"
 
@@ -43,7 +42,7 @@ func NewDomiconStorageService(ctx context.Context, config DomiconStorageServiceC
 }
 
 func (s *DomiconStorageService) GetByHash(ctx context.Context, hash common.Hash) ([]byte, error) {
-	log.Trace("das.DomiconStorageService.GetByHash", "hash", pretty.PrettyHash(hash))
+	log.Trace("das.DomiconStorageService.GetByHash", "hash", hash.String())
 
 	id := 0
 	method := "eth_getFileDataByCommitment"
@@ -93,77 +92,6 @@ func (s *DomiconStorageService) String() string {
 	return "DomiconStorageService"
 }
 
-//	func NewDomiconStorageServiceFromURL(url string) (*DomiconStorageService, error) {
-//		if !(strings.HasPrefix(url, "http://") || strings.HasPrefix(url, "https://")) {
-//			return nil, fmt.Errorf("protocol prefix 'http://' or 'https://' must be specified for DomiconStorageService; got '%s'", url)
-//
-//		}
-//		return &DomiconStorageService{
-//			url: url,
-//		}, nil
-//	}
-//
-//	func (c *DomiconStorageService) GetByHash(ctx context.Context, hash common.Hash) ([]byte, error) {
-//		res, err := http.Get(c.url + getByHashRequestPath + EncodeStorageServiceKey(hash))
-//		if err != nil {
-//			return nil, err
-//		}
-//		if res.StatusCode != http.StatusOK {
-//			return nil, fmt.Errorf("HTTP error with status %d returned by server: %s", res.StatusCode, http.StatusText(res.StatusCode))
-//		}
-//
-//		body, err := io.ReadAll(res.Body)
-//		if err != nil {
-//			return nil, err
-//		}
-//
-//		var response RestfulDasServerResponse
-//		err = json.Unmarshal(body, &response)
-//		if err != nil {
-//			return nil, err
-//		}
-//
-//		decoder := base64.NewDecoder(base64.StdEncoding, bytes.NewReader([]byte(response.Data)))
-//		decodedBytes, err := io.ReadAll(decoder)
-//		if err != nil {
-//			return nil, err
-//		}
-//		if !dastree.ValidHash(hash, decodedBytes) {
-//			return nil, arbstate.ErrHashMismatch
-//		}
-//
-//		return decodedBytes, nil
-//	}
 func (c *DomiconStorageService) HealthCheck(ctx context.Context) error {
-	//res, err := http.Get(c.url + healthRequestPath)
-	//if err != nil {
-	//	return err
-	//}
-	//if res.StatusCode != http.StatusOK {
-	//	return fmt.Errorf("HTTP error with status %d returned by server: %s", res.StatusCode, http.StatusText(res.StatusCode))
-	//}
 	return nil
 }
-
-//
-//func (c *DomiconStorageService) ExpirationPolicy(ctx context.Context) (arbstate.ExpirationPolicy, error) {
-//	res, err := http.Get(c.url + expirationPolicyRequestPath)
-//	if err != nil {
-//		return -1, err
-//	}
-//	if res.StatusCode != http.StatusOK {
-//		return -1, err
-//	}
-//	body, err := io.ReadAll(res.Body)
-//	if err != nil {
-//		return -1, fmt.Errorf("HTTP error with status %d returned by server: %s", res.StatusCode, http.StatusText(res.StatusCode))
-//	}
-//
-//	var response RestfulDasServerResponse
-//	err = json.Unmarshal(body, &response)
-//	if err != nil {
-//		return -1, err
-//	}
-//
-//	return arbstate.StringToExpirationPolicy(response.ExpirationPolicy)
-//}
--- das/domicon_storage_service_test.go
+++ das/domicon_storage_service_test.go
@@ -1,1 +1,25 @@
 package das
+
+import (
+	"context"
+	"testing"
+	"time"
+)
+
+func TestDomiconStorageServiceGet(t *testing.T) {
+	enableLogging()
+	ctx := context.Background()
+	svc, err := NewDomiconStorageService(ctx,
+		DomiconStorageServiceConfig{
+			Enable:      true,
+			ReadTimeout: time.Minute,
+			Peer:        "http://54.177.136.244:8545",
+		})
+	defer svc.Close(ctx)
+	Require(t, err)
+	//returnedData, err := svc.GetByHash(ctx, common.BytesToHash(hash))
+	//Require(t, err)
+	//if !bytes.Equal(data, returnedData) {
+	//	Fail(t, "Returned data didn't match!")
+	//}
+}
